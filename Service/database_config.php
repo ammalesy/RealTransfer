c20cc6fa1593dee609d88a824e7582a25148fca4
nui <nui@xxx.com>
2016-06-20 15:17:27 +0700

    AMAZON

--- Service/database_config.php
+++ Service/database_config.php
@@ -11,13 +11,13 @@
 		}
 
 		public static function condo_common() {
-			$connect = mysqli_connect("localhost","root","","condo_common");
+			$connect = mysqli_connect("localhost","ammales","9B215ffa","condo_common");
 			mysqli_set_charset($connect ,"utf8");
 			return $connect;
 		}
 		public static function connect_db_by_dbName($dbName) {
 
-			$connect = mysqli_connect("localhost","root","",$dbName);
+			$connect = mysqli_connect("localhost","ammales","9B215ffa",$dbName);
 			mysqli_set_charset($connect ,"utf8");
 			return $connect;
 		}
